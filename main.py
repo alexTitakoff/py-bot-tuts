92bfe5762d70c30db42dc2156875906077a34840
alex <alex.titakoff@yandex.ru>
2017-11-24 23:45:47 +0300

    simple

--- main.py
+++ main.py
@@ -22,16 +22,43 @@ def get_updates():
 
 # @bot.message_handler(content_types=['text'])
 # @bot.message_handler(func=lambda message: True, content_types=['text'])
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+@bot.message_handler(content_types=['text'])
 def handle_text(message):
-    print('-- handler')
+    answer = "Привет чтобы начать играть наберите комманду /game_start"
     if message.text == 'a':
+        answer = 'b'
         bot.send_message(message.chat.id, 'b')
+        log(message,answer)
     elif message.text == 'b':
+        answer = 'c'
         bot.send_message(message.chat.id, 'c')
+        log(message, answer)
     else:
         bot.send_message(message.chat.id, 'Хелло')
+        log(message, answer)
 
+
+
+def log(message, answer):
+    print('\n ------')
+    from datetime import datetime
+    print(datetime.now())
+    print("Сообщение от {0} {1}. (id={2}) \n Текст - {3}".format(message.from_user.first_name,
+                                                                  message.from_user.last_name,
+                                                                  str(message.from_user.id),
+                                                                  message.text))
+    print(answer)
+
+
+
+
+@bot.message_handler(commands=['help'])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Это комманда помощи')
+
+@bot.message_handler(commands=['game'])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'игра городки началась')
 
 
 
